dcdc5c3be8e4af4c819a21bba3a1bedf5809c0b8
Yannik Wissner <wissner@neoskop.de>
2019-05-23 09:27:15 +0200

    feat: add hashing of external script files

--- src/node.api.js
+++ src/node.api.js
@@ -4,9 +4,10 @@ import { defaultOptions } from './helpers/options';
 import checkValid from './helpers/checkValid';
 import createAttrString from './helpers/createAttrString';
 import createPolicyObject from './helpers/createPolicyObject';
+import fs from 'fs';
 
 export default options => ({
-  beforeDocumentToFile: (html, { stage }) => {
+  beforeDocumentToFile: (html, { stage, config: { paths } }) => {
     if (stage === 'prod') {
       // merge options with defaultOptions, so all optional fields are also covered
       options = {
@@ -23,14 +24,33 @@ export default options => ({
       // gogo, find all inline tags in need of hashing
       const shas = {};
 
-      const scriptTags = $('script:not([src])');
-      shas['script-src'] = scriptTags
-        .map((idx, elem) => getHash($(elem, options.hashingMethod).html()))
+      const inlineScriptTags = $('script:not([src])');
+      shas['script-src'] = inlineScriptTags
+        .map((_, elem) => getHash($(elem, options.hashingMethod).html()))
         .get();
-      const styleTags = $('style:not([href])');
-      shas['style-src'] = styleTags
-        .map((idx, elem) => getHash($(elem, options.hashingMethod).html()))
+      const inlineStyleTags = $('style:not([href])');
+      shas['style-src'] = inlineStyleTags
+        .map((_, elem) => getHash($(elem, options.hashingMethod).html()))
         .get();
+
+      // now, find all the external scripts, we want to hash those aswell
+      const scriptTags = $('script[src]');
+      shas['script-src'].push(
+        ...scriptTags
+          .map((_, elem) => {
+            const { src } = elem.attribs;
+            const { ASSETS: assetsPath } = paths;
+            try {
+              const filepath = `${assetsPath}${src}`;
+              const file = fs.readFileSync(filepath, 'utf8');
+
+              return getHash(file, options.hashingMethod);
+            } catch (error) {
+              console.error(error);
+            }
+          })
+          .get()
+      );
 
       const metaTag = cheerio.load(
         '<meta http-equiv="Content-Security-Policy">'
